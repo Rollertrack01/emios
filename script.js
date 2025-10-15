5d2ed22cec53d69df1609e028b5aaba378959a6d
Rollertrack01 <gentilfranciscomaia@gmail.com>
2025-10-15 10:07:57 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// Menu mobile
+const mobileMenu = document.getElementById('mobile-menu');
+const navLinks = document.querySelector('nav ul');
+
+mobileMenu.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+});
